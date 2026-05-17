5e50277a3161e59d8304959a0ec410067ebc728a
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-05-17 15:33:57 +0600

    Added CORE app and created Patient model

---
+++ core/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.conf import settings
+class Patient(models.model):
+    #Gender choices
+    GENDER_CHOICES = (('M', 'Male'), ('F', 'Female'))
+
+    name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    age = models.IntegerField(max_length=3)
+    address = models.TextField()
+    phone = models.CharField(max_length=15)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f'{self.name} (ID: {self.display_id})'
+
+  
+    
