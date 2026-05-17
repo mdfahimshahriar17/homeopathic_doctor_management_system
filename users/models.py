5f4a8fd94dad320b186f99e3037947242cf3dfd5
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-05-17 10:18:58 +0600

    Initialized project with base.html

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+     # Role choices
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('doctor', 'Doctor'),
+        ('compounder', 'Compounder'),
+        ('receptionist', 'Receptionist'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)  # record creation timestamp
+    updated_at = models.DateTimeField(auto_now=True)      # record last update
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
+
