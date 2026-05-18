51ea305d1d8104de5d7dc248eaea9bb2b1862077
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-05-18 08:53:56 +0600

    Created patient_list

--- core/urls.py
+++ core/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('register_patient/', views.register_patient, name='register_patient'),
+    path('patient/list/', views.patient_list, name='patient_list'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from . import forms
-
+from . import models
 
 def register_patient(request):
     if request.method == 'POST':
@@ -14,3 +14,9 @@ def register_patient(request):
         form = forms.PatientForm()
 
     return render(request, 'core/create_patient.html', {'form' : form})
+
+
+def patient_list(request):
+    patients = models.Patient.objects.all()
+
+    return render(request, 'core/patient_list.html', {'patients' : patients})
